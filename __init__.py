477b06fc822b2a377235282a4d143caccb3dd6cc
Rohit Sinha <rohitsinha149489@gmail.com>
2021-04-20 22:25:07 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,73 @@
+from flask import Flask, jsonify, request, flash, redirect, url_for
+from mysql.connector import connect, Error
+from flask_cors import CORS, cross_origin
+
+STRING_FAIL = 'fail'
+STRING_SUCCESS = 'success'
+
+def get_db_connection():
+    try:
+        connection = connect(host="localhost", user="<username>", password="<password>", database="<DB>")
+        app.logger.info('Database connected Successfully %s', connection)
+    except Error as e:
+        app.logger.error('Error Occured while connecting to Database - %s', e)
+        return None
+    return connection
+
+def get_serialize_data(fields, results):
+
+    column_list = []
+
+    for i in fields:
+        column_list.append(i[0])
+
+    jsonData_list = []
+    for row in results:
+        data_dict = {}
+        for i in range(len(column_list)):
+            data_dict[column_list[i]] = row[i]
+        data_dict['phoneNumber'] = data_dict['phoneNumber'].split('/')
+        jsonData_list.append(data_dict)
+    return jsonData_list
+
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/")
+@cross_origin()
+def hello():
+    return "Hello, Flask!"
+
+@app.route('/api/distributors', methods=['GET'])
+@cross_origin()
+def get_all_medicine():
+
+    medicine = request.args.get('medicine')
+    city = request.args.get('city')
+
+    app.logger.info('Fetching data for medicine - %s and city - %s', medicine, city)
+
+    conn = get_db_connection()
+    if conn:
+        cursor = conn.cursor()
+        get_query = """select * from distributors left join medicine on distributors.distributorID = medicine.distributorID where city=%s and medicineName=%s""" % (city, medicine)
+        cursor.execute(get_query)
+        medicine_details = cursor.fetchall()
+        fields_list = cursor.description
+    else:
+        app.logger.error('Counld not get data from table as connection is - %s', conn)
+        return jsonify(meta=STRING_FAIL)
+
+    cursor.close()
+    conn.close()
+
+    json_data_list = get_serialize_data(fields_list, medicine_details)
+
+    response = jsonify(meta=STRING_SUCCESS, response=json_data_list)
+    return response
+
+
+if __name__ == '__main__' :
+    app.run(debug=True)
